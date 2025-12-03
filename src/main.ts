8e0eb188c2fc3f4562c214e9e42cf2faf2ea4416
Piotr Migdal <pmigdal@gmail.com>
2025-12-03 19:08:26 +0100

    plausible, katex as package not from cdn

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import Plausible from "plausible-tracker";
+import "katex/dist/katex.min.css";
 
-createApp(App).mount('#app')
+// Initialize Plausible Analytics
+const { enableAutoPageviews } = Plausible({
+  domain: "p.migdal.pl",
+  apiHost: "https://plausible.io",
+});
+
+enableAutoPageviews();
+
+createApp(App).mount("#app");
